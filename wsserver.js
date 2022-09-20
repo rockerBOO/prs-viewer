037781a4c1a5e0fececfed5fab7c01eaa89f41cb
Dave Lage (rockerBOO) <rockerboo@gmail.com>
2022-09-20 02:07:59 -0400

    cleanup wsserver

--- wsserver.js
+++ wsserver.js
@@ -1,37 +1,17 @@
 import chokidar from "chokidar";
-
-import fs from "fs";
+import { readdirSync } from "fs";
 import { WebSocketServer } from "ws";
 
-// initialize the WebSocket server instance
 const wss = new WebSocketServer({ port: 8999 });
 
 const prsOut = "/mnt/900/builds/prs/out";
 
-// const getAllFiles = function (dirPath, arrayOfFiles) {
-//   const path = fs.readdirSync(dirPath);
-//
-//   arrayOfFiles = arrayOfFiles || [];
-//
-//   const [, dir, file] = path.replace(prsOut, "").split("/");
-//
-//   files.forEach(function (file) {
-//     if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-//       arrayOfFiles = getAllFiles(dirPath + "/" + file, arrayOfFiles);
-//     } else {
-//       arrayOfFiles.push({ dir: dir, file });
-//     }
-//   });
-//
-//   return arrayOfFiles;
-// };
-
 const getAllFiles = () => {
-  const dirs = fs.readdirSync(prsOut);
+  const dirs = readdirSync(prsOut);
 
   return dirs
     .map((dir) => {
-      const files = fs.readdirSync(`${prsOut}/${dir}`);
+      const files = readdirSync(`${prsOut}/${dir}`);
       return files.map((file) => ({ dir, file }));
     })
     .reduce((acc, dir) => {
@@ -42,11 +22,14 @@ const getAllFiles = () => {
 wss.on("connection", (ws) => {
   const files = getAllFiles();
   ws.send(JSON.stringify({ event: "addBatch", files: files }));
+
   chokidar.watch(prsOut, { ignoreInitial: true }).on("all", (event, path) => {
     const [, dir, file] = path.replace(prsOut, "").split("/");
-    console.log(event, dir, file);
+
     ws.send(JSON.stringify({ event, dir, file }), (err) => {
       console.log("error", err);
     });
   });
 });
+
+console.log('Listening on ws://localhost:8999')
