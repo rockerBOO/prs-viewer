eb25c4ba565a0340a6336bafc04f220b8d2a380e
Dave Lage (rockerBOO) <rockerboo@gmail.com>
2022-12-09 23:40:01 -0500

    make http server more configurable

--- index.js
+++ index.js
@@ -4,34 +4,43 @@ import cors from "cors";
 import pkg from "really-relaxed-json";
 const { toJson } = pkg;
 import { env } from "node:process";
-import path from "node:path";
+import { readdir } from "node:fs/promises"
 
-const prsOut = env.PRS_VIEWER_OUT ?? "/mnt/900/builds/prs/out";
+// Set to the output directory
+// PRS_VIEWER_OUT=~/stablediffusion/outputs/txt2img-samples
+const PRS_OUT = env.PRS_VIEWER_OUT ?? "/mnt/900/builds/prs/out";
+
+// Set the listening port of the HTTP server for the REST API
+// and image delivery
+// PRS_HTTP_PORT=3000
+const PRS_HTTP_PORT = env.PRS_HTTP_PORT ?? 3000;
 
 const app = express();
 
 app.use(cors());
-app.use(express.static(prsOut));
+app.use(
+  express.static(PRS_OUT, {
+    maxAge: "1h",
+  })
+);
 
 app.get("/files", (_req, res) => {
-  const dirs = fs.readdirSync(prsOut);
-
-  const files = dirs
-    .map((dir) => {
-      const files = fs.readdirSync(`${prsOut}/${dir}`);
-
-      return { [dir]: files };
-    })
-    .reduce((acc, set) => {
-      return { ...acc, ...set };
-    }, {});
-
-  res.json(files);
+  readdir(PRS_OUT).then((dirs) => {
+    Promise.all(
+      dirs.map(async (dir) => {
+        return readdir(`${PRS_OUT}/${dir}`).then((files) => {
+          return { [dir]: files };
+        });
+      })
+    ).then((dirsOfFiles) => {
+      res.json(dirsOfFiles);
+    });
+  });
 });
 
 app.get("/dir/:dir", (req, res) => {
   const dir = req.params["dir"];
-  const files = fs.readdirSync(`${prsOut}/${dir}`);
+  const files = fs.readdirSync(`${PRS_OUT}/${dir}`);
 
   res.json(files);
 });
@@ -39,15 +48,15 @@ app.get("/dir/:dir", (req, res) => {
 app.get("/settings/:dir/:file", (req, res) => {
   const dir = req.params["dir"];
   const file = req.params["file"];
-	const filepath = `${prsOut}/${dir}/${file}`;
-	if (!fs.existsSync(filepath)) {
-		console.log(`404 ${filepath}`)
-		return;
-	}
+  const filepath = `${PRS_OUT}/${dir}/${file}`;
+  if (!fs.existsSync(filepath)) {
+    console.log(`404 ${filepath}`);
+    return;
+  }
   const settings = fs.readFileSync(filepath);
   res.json(JSON.parse(toJson(settings.toString())));
 });
 
-app.listen(3000);
+app.listen(PRS_HTTP_PORT);
 
 console.log("Listening on http://localhost:3000");
